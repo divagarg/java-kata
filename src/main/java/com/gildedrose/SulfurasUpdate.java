3f54e5d3b059fd9f04c7b1d0f60d8b0e0c12e85c
Divya Garg <garg.diva@gmail.com>
2022-12-09 10:22:54 +0100

    declared constants

--- src/main/java/com/gildedrose/SulfurasUpdate.java
+++ src/main/java/com/gildedrose/SulfurasUpdate.java
@@ -1,9 +1,12 @@
 package com.gildedrose;
 
 public class SulfurasUpdate implements ItemUpdateInterface{
+
+    static final int QUALITY = 80;
+    static final int SELL_IN = 0;
     @Override
     public void updateItem(Item item) {
-            item.quality = 80;
-            item.sellIn = 0;
+            item.quality = QUALITY;
+            item.sellIn = SELL_IN;
     }
 }
